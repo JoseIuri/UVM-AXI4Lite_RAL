47dc04685572162222275f3dfdca18d610f98677
Jiss <jissjoseph14@gmail.com>
2025-01-07 16:09:32 +0000

    constructor modified to enable coverage

--- tb/reg_lib/ral_block_traffic_cfg.sv
+++ tb/reg_lib/ral_block_traffic_cfg.sv
@@ -1,4 +1,3 @@
-// These registers are grouped together to form a register block called "cfg"
 class ral_block_traffic_cfg extends uvm_reg_block;
   
   `uvm_object_utils(ral_block_traffic_cfg)
@@ -10,32 +9,37 @@ class ral_block_traffic_cfg extends uvm_reg_block;
   uvm_reg_map         reg_map;
  
  
-  function new(string name = "traffic_cfg");
-    super.new(name, build_coverage(UVM_NO_COVERAGE));
+  function new(string name = "ral_block_traffic_cfg");
+    super.new(name, UVM_CVR_ALL);
+      `uvm_info("RAL_BLOCK", $sformatf("Creating instance: %s", name), UVM_LOW);
   endfunction
  
+  
   virtual function void build();
-    this.reg_map = create_map("", 0, 4, UVM_LITTLE_ENDIAN, 0);
+    this.reg_map = create_map("", 0, 8, UVM_LITTLE_ENDIAN, 0);
     
     this.ctrl = ral_cfg_ctl::type_id::create("ctrl",,get_full_name());
-    this.ctrl.configure(this, null, "");
     this.ctrl.build();
+    this.ctrl.configure(this, null, "");   
     this.reg_map.add_reg(this.ctrl, `UVM_REG_ADDR_WIDTH'h0, "RW", 0);
  
  
     this.timer[0] = ral_cfg_timer::type_id::create("timer[0]",,get_full_name());
-    this.timer[0].configure(this, null, "");
     this.timer[0].build();
+    this.timer[0].configure(this, null, "");
+    
     this.reg_map.add_reg(this.timer[0], `UVM_REG_ADDR_WIDTH'h4, "RW", 0);
  
     this.timer[1] = ral_cfg_timer::type_id::create("timer[1]",,get_full_name());
-    this.timer[1].configure(this, null, "");
     this.timer[1].build();
+    this.timer[1].configure(this, null, "");
+    
     this.reg_map.add_reg(this.timer[1], `UVM_REG_ADDR_WIDTH'h8, "RW", 0);
  
     this.stat = ral_cfg_stat::type_id::create("stat",,get_full_name());
-    this.stat.configure(this, null, "");
     this.stat.build();
+    this.stat.configure(this, null, "");
+    
     this.reg_map.add_reg(this.stat, `UVM_REG_ADDR_WIDTH'hc, "RO", 0);
 
     lock_model();
