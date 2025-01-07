f51f1a857e8fb934022897ae883d895c05c329ff
Jiss <jissjoseph14@gmail.com>
2025-01-07 16:06:21 +0000

    Added coverage constructs and reset value

--- tb/reg_lib/ral_cfg_ctl.sv
+++ tb/reg_lib/ral_cfg_ctl.sv
@@ -1,80 +1,58 @@
-// Register definition for the register called "ctl"
+import uvm_pkg::*;           // Import UVM package
+`include "uvm_macros.svh"    // Include UVM macros
 class ral_cfg_ctl extends uvm_reg;
-    rand uvm_reg_field mod_en;      // Enables the module
-    rand uvm_reg_field bl_yellow;   // Blinks yellow
-    rand uvm_reg_field bl_red;      // Blinks red
-    rand uvm_reg_field profile;     // 1 : Peak, 0 : Off-Peak
+    rand uvm_reg_field mod_en;
+    rand uvm_reg_field bl_yellow;
+    rand uvm_reg_field bl_red;
+    rand uvm_reg_field profile;
 
     `uvm_object_utils(ral_cfg_ctl)
-// new code start
 
-////////////////////////adding coverpoints     
+ 
   
- covergroup mod_cov;
-    
-   option.per_instance = 1;
-    
-    
-   coverpoint mod_en 
-    {
-      bins low = {0};
-      bins high = {1};
+    // Define the covergroup
+  covergroup cg(string name = "default_name");
+    option.per_instance=1;
+    option.auto_bin_max=2;
+    coverpoint mod_en.get() {
+     
+      bins value[] = {0,1};
+    }    // Track the values of mod_en
+    coverpoint bl_yellow.get() {
+   bins yellow_value[] = {0, 1};
     }
+    coverpoint bl_red.get() {
+   bins red_value[] = {0, 1};
+}
 
-    coverpoint bl_yellow;
-    coverpoint b1_red;
-    coverpoint profile;
-     
-  endgroup
-  
  
-  ////////////////checking coverage and adding new method to covergroup
+    endgroup
   
-  function new (string name = "ral_cfg_ctl");
-    super.new(name,32,UVM_CVR_FIELD_VALS);
-    
-    if(has_coverage(UVM_CVR_FIELD_VALS))
-      mod_en = new();
-    
-  endfunction
- 
-//////////////////////////////   implementation of sample and sample_Values  
-  
-  
-  virtual function void sample(uvm_reg_data_t data,
-                                uvm_reg_data_t byte_en,
-                                bit            is_read,
-                                uvm_reg_map    map);
-    
-         mod_en.sample();
-   endfunction
-  
-  
-   virtual function void sample_values();
-    super.sample_values();
-     
-    mod_en.sample();
-     
-   endfunction
+     function new(string name = "ral_cfg_ctl");
+        super.new(name, 8, UVM_CVR_ALL);
+       cg = new(); // Initialize the covergroup
+           `uvm_info("RAL_CFG_CTL", $sformatf("Creating instance: %s", name), UVM_LOW);
 
-
-    //new code end: jissjoseph1997
-
-   /* function new(string name = "traffic_cfg_ctrl");
-        super.new(name, 32, build_coverage(UVM_NO_COVERAGE));
-    endfunction: new */
-
-    // Build all register field objects
-    virtual function void build();
-        this.mod_en     = uvm_reg_field::type_id::create("mod_en",,   get_full_name());
-        this.bl_yellow  = uvm_reg_field::type_id::create("bl_yellow",,get_full_name());
-        this.bl_red     = uvm_reg_field::type_id::create("bl_red",,   get_full_name());
-        this.profile    = uvm_reg_field::type_id::create("profile",,  get_full_name());
-
-        // configure(parent, size, lsb_pos, access, volatile, reset, has_reset, is_rand, individually_accessible); 
-        this.mod_en.configure(this, 1, 0, "RW", 0, 1'h0, 1, 0, 0);
-        this.bl_yellow.configure(this, 1, 1, "RW", 0, 3'h4, 1, 0, 0);
-        this.bl_red.configure(this, 1, 2, "RW", 0, 1'h0, 1, 0, 0);
-        this.profile.configure(this, 1, 3, "RW", 0, 1'h0, 1, 0, 0);
     endfunction
+  
+     // Sample the covergroup whenever a transaction occurs
+    virtual function void sample_coverage();
+        $display("Sampling coverage for ral_cfg_ctl");
+        cg.sample();
+    endfunction 
+
+    virtual function void build();
+        this.mod_en = uvm_reg_field::type_id::create("mod_en", , get_full_name());
+        this.bl_yellow = uvm_reg_field::type_id::create("bl_yellow", , get_full_name());
+        this.bl_red = uvm_reg_field::type_id::create("bl_red", , get_full_name());
+        this.profile = uvm_reg_field::type_id::create("profile", , get_full_name());
+
+        this.mod_en.configure(this, 1, 0, "RW", 0, 1'h0, 1, 1, 1);
+        this.bl_yellow.configure(this, 1, 1, "RW", 0, 1'h0, 1, 1, 1);
+        this.bl_red.configure(this, 1, 2, "RW", 0, 1'h0, 1, 1, 1);
+        this.profile.configure(this, 1, 3, "RW", 0, 1'h0, 1, 1, 1);
+
+        this.set_reset(32'h00000000, "SOFT"); // Reset value for ctl_reg
+    endfunction
+  
 endclass
