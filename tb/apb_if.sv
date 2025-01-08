e50cf66e52667d72d6966775aca08e45df04cec6
Jiss <jissjoseph14@gmail.com>
2025-01-08 10:53:43 +0000

    added assertions for address check

--- tb/apb_if.sv
+++ tb/apb_if.sv
@@ -6,4 +6,15 @@ interface apb_if (input pclk, input presetn); // presetn as input
     logic psel;
     logic penable;
     logic pready;   // If pready is used
+  	logic pslverr;
+  // Assertion: pslverr should be asserted for invalid addresses
+property invalid_address_check;
+    @(posedge pclk) (paddr inside {0, 4, 8}) or pslverr == 1'b1;
+endproperty
+
+assert property (invalid_address_check) 
+else $error("Invalid address detected !");
+
+
+
 endinterface
