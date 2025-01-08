bc9f6c1e99e4cde9c352c4ba4c1012b8233d2e30
Jiss <jissjoseph14@gmail.com>
2025-01-08 07:23:20 +0000

    added reset value

--- tb/reg_lib/ral_cfg_stat.sv
+++ tb/reg_lib/ral_cfg_stat.sv
@@ -1,15 +1,17 @@
 class ral_cfg_stat extends uvm_reg;
-    uvm_reg_field state;    // Current state of the design
+    uvm_reg_field state;
 
     `uvm_object_utils(ral_cfg_stat)
+
     function new(string name = "ral_cfg_stat");
-        super.new(name, 32, build_coverage(UVM_NO_COVERAGE));
+        super.new(name, 8, UVM_CVR_ALL);
     endfunction
 
     virtual function void build();
-        this.state = uvm_reg_field::type_id::create("state",, get_full_name());
+        this.state = uvm_reg_field::type_id::create("state", , get_full_name());
+        this.state.configure(this, 2, 0, "RO", 0, 1'h0, 1, 1, 1);
 
-        // configure(parent, size, lsb_pos, access, volatile, reset, has_reset, is_rand, individually_accessible); 
-        this.state.configure(this, 2, 0, "RO", 0, 1'h0, 0, 0, 0);
+        this.set_reset(32'h00000000, "SOFT"); // Reset value for stat_reg
     endfunction
 endclass
+
