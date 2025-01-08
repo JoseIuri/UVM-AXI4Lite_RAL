c6b8e33078518e6b4729255333237a0db56a6c60
Jiss <jissjoseph14@gmail.com>
2025-01-08 08:20:54 +0000

    Added reset sequence to the read write test

--- tb/reg_rw_test.sv
+++ tb/reg_rw_test.sv
@@ -1,22 +1,30 @@
 class reg_rw_test extends base_test;
-   `uvm_component_utils (reg_rw_test)
-    
-    my_sequence m_seq;
-   
-   function new (string name="reg_rw_test", uvm_component parent);
-      super.new (name, parent);
+   `uvm_component_utils(reg_rw_test)
+
+   my_sequence m_seq;
+   reset_seq   rst_seq;
+
+   function new(string name = "reg_rw_test", uvm_component parent);
+      super.new(name, parent);
    endfunction
 
    virtual task main_phase(uvm_phase phase);
-
       phase.raise_objection(this);
+
+      // Create and run the reset sequence
+      rst_seq = reset_seq::type_id::create("rst_seq");
+      rst_seq.start(null); // Null sequencer for direct execution of reset
+
+      // Create and run the main test sequence
       m_seq = my_sequence::type_id::create("m_seq");
 
-      m_seq.model = m_ral_model;
+      // Pass the Register Model to the Sequence
+      m_seq.model = m_env.m_ral_model;
 
-
+      // Run the main test sequence
       m_seq.start(m_env.m_agent.m_seqr);
 
       phase.drop_objection(this);
    endtask
 endclass
+
